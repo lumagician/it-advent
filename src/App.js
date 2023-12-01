db688fe62322b64c8b705ccd77d9a6d33dac71b9
Leo Hofer <hfl2@bfh.ch>
2023-12-01 11:09:46 +0100

    Add VSCode settings and update package dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Typography,
+  Card,
+  CardContent,
+  Grid,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+
+const darkChristmasTheme = createTheme({
+  palette: {
+    type: "dark", // Use dark mode
+    primary: {
+      main: "#c62828", // Deep red
+    },
+    secondary: {
+      main: "#2e7d32", // Dark green
+    },
+  },
+});
 
 function App() {
+  const [data, setData] = useState([]);
+  const [revealedDate, setRevealedDate] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://raw.githubusercontent.com/lumagician/it-advent/master/data/calendar.json"
+        );
+        const jsonData = await response.json();
+        setData(jsonData.topics);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const revealDate = (date) => {
+    setRevealedDate(date);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkChristmasTheme}>
+      <Container>
+        <Typography variant="h2" align="center" gutterBottom>
+          IT Advent Calendar
+        </Typography>
+
+        <Grid container spacing={3}>
+          {data.map((topic) => {
+            const isPastDate = new Date(topic.date) < new Date();
+            const isCurrentDate = topic.date === revealedDate;
+            const isFutureDate = new Date(topic.date) > new Date();
+
+            return (
+              <Grid item xs={12} md={6} lg={4} key={topic.date}>
+                <Card
+                  style={{
+                    cursor: isFutureDate ? "not-allowed" : "pointer",
+                    backgroundColor: isCurrentDate
+                      ? darkChristmasTheme.palette.secondary.main
+                      : "",
+                    height: "100%", // Set a fixed height for each Card
+                    minHeight: "200px", // Set a minimum height if needed
+                  }}
+                  onClick={() => !isFutureDate && revealDate(topic.date)}
+                >
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {topic.name}
+                    </Typography>
+                    <Typography color="text.secondary">{topic.date}</Typography>
+                    <Typography>
+                      {isPastDate || isCurrentDate
+                        ? topic.explanation.english
+                        : "Future date - Cannot be revealed"}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
